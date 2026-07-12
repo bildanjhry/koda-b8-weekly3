66b3a65ca5df5c750460129e5b906af542f6b66a
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-12 16:23:27 +0700

    feat: add money format utility fo displaying price

--- utils/moneyFormat.go
+++ utils/moneyFormat.go
@@ -1,5 +1,19 @@
 package utils
 
-func MoneyFormat() {
+import "strconv"
 
+func MoneyFormat(total *int) string {
+	totalStr := strconv.Itoa(*total)
+	format := ""
+	switch len(totalStr) {
+	case 4:
+		format = totalStr[:1] + "." + totalStr[1:]
+	case 5:
+		format = totalStr[:2] + "." + totalStr[2:]
+	case 6:
+		format = totalStr[:3] + "." + totalStr[3:]
+	case 7:
+		format = totalStr[:1] + "." + totalStr[1:4] + "." + totalStr[4:]
+	}
+	return format
 }
